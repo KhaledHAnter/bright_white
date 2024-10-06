a4731c91acbb92c51218ffe6141b7c565776d740
KhaledHAnter <khaldntr11@gmail.com>
2024-10-06 15:41:58 +0300

    finish pdf feature

--- lib/features/home/ui/widgets/home_view_body.dart
+++ lib/features/home/ui/widgets/home_view_body.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:bright_white/core/theming/colors.dart';
 import 'package:bright_white/core/theming/styles.dart';
 import 'package:bright_white/generated/l10n.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
@@ -13,6 +13,7 @@ import 'dart:io';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:pdf/pdf.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
@@ -434,74 +435,126 @@ class _HomeViewBodyState extends State<HomeViewBody> {
     // Load the Arabic font from assets
     final fontData = await rootBundle.load('assets/fonts/Cairo-Regular.ttf');
     final ttf = pw.Font.ttf(fontData);
+    final date = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
     final pdf = pw.Document();
 
-    // Add content to the PDF
+    // Add content to the PDF using MultiPage to accommodate multiple pages
     pdf.addPage(
-      pw.Page(
+      pw.MultiPage(
         pageFormat: PdfPageFormat.a4,
         build: (context) {
-          return pw.Directionality(
-            textDirection:
-                pw.TextDirection.rtl, // Set the text direction to RTL
-            child: pw.Column(
-              crossAxisAlignment: pw.CrossAxisAlignment.start,
-              children: [
-                pw.Text(
-                  'قائمة العملاء',
-                  style: pw.TextStyle(
-                      font: ttf, fontSize: 24, fontWeight: pw.FontWeight.bold),
-                ),
-                pw.SizedBox(height: 16),
-                // Iterate over all customers and add their details to the PDF
-                for (var customer in _customers) ...[
-                  pw.Text(
-                    'الاسم: ${customer.name}',
-                    style: pw.TextStyle(font: ttf, fontSize: 18),
+          return [
+            pw.Directionality(
+              textDirection: pw.TextDirection.rtl,
+              child: pw.Column(
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Row(
+                    children: [
+                      pw.Text(
+                        'قائمة العملاء',
+                        style: pw.TextStyle(
+                            font: ttf,
+                            fontSize: 18,
+                            fontWeight: pw.FontWeight.bold),
+                      ),
+                      pw.Spacer(),
+                      pw.Text(
+                        'التاريخ : $date',
+                        style: pw.TextStyle(
+                            font: ttf,
+                            fontSize: 18,
+                            fontWeight: pw.FontWeight.bold),
+                      ),
+                      pw.Spacer(),
+                      pw.Text(
+                        ' المجموع : ${_calculateTotalMoneyFormatted()}',
+                        style: pw.TextStyle(
+                            font: ttf,
+                            fontSize: 18,
+                            color: PdfColor.fromHex('#8B0000'),
+                            fontWeight: pw.FontWeight.bold),
+                      ),
+                    ],
                   ),
-                  pw.Text(
-                    'الهاتف: ${customer.phone}',
-                    style: pw.TextStyle(font: ttf, fontSize: 16),
-                  ),
-                  pw.Text(
-                    'المبلغ المستحق: ${NumberFormat('#,##0').format(customer.money)}',
-                    style: pw.TextStyle(font: ttf, fontSize: 16),
-                  ),
-                  if (customer.transactions.isNotEmpty) ...[
-                    pw.Text(
-                      'المعاملات:',
-                      style: pw.TextStyle(
-                          font: ttf,
-                          fontSize: 16,
-                          fontWeight: pw.FontWeight.bold),
-                    ),
-                    pw.Column(
-                      crossAxisAlignment: pw.CrossAxisAlignment.start,
+
+                  pw.SizedBox(height: 16),
+                  // Iterate over all customers and add their details to the PDF
+                  for (var customer in _customers) ...[
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
                       children: [
-                        for (var transaction in customer.transactions) ...[
-                          pw.Text(
-                            '- التاريخ: ${transaction.dateTime.toLocal().toString().split(' ')[0]}, النوع: ${transaction.type}, المبلغ: ${transaction.amount}, الوصف: ${transaction.description ?? 'N/A'}',
-                            style: pw.TextStyle(font: ttf, fontSize: 14),
+                        pw.Text(
+                          'الاسم: ${customer.name}',
+                          style: pw.TextStyle(font: ttf, fontSize: 14),
+                        ),
+                        pw.Text(
+                          'الهاتف: ${customer.phone}',
+                          style: pw.TextStyle(font: ttf, fontSize: 12),
+                        ),
+                        pw.Text(
+                          'المبلغ المستحق: ${NumberFormat('#,##0').format(customer.money)}',
+                          style: pw.TextStyle(
+                            font: ttf,
+                            fontSize: 12,
+                            fontWeight: pw.FontWeight.bold,
+                            color: PdfColor.fromHex('#06402B'),
                           ),
-                        ],
+                        ),
                       ],
                     ),
+                    if (customer.transactions.isNotEmpty) ...[
+                      pw.SizedBox(height: 4),
+                      pw.Align(
+                        alignment: pw.Alignment.center,
+                        child: pw.Text(
+                          'المعاملات:',
+                          style: pw.TextStyle(
+                              font: ttf,
+                              fontSize: 14,
+                              fontWeight: pw.FontWeight.bold),
+                        ),
+                      ),
+                      pw.Column(
+                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                        children: [
+                          for (var transaction in customer.transactions) ...[
+                            pw.Text(
+                              '- التاريخ: ${transaction.dateTime.toLocal().toString().split(' ')[0]},         النوع: ${transaction.type},         المبلغ: ${NumberFormat('#,##0').format(transaction.amount)}         الوصف: ${transaction.description ?? 'N/A'}',
+                              style: pw.TextStyle(font: ttf, fontSize: 10),
+                            ),
+                          ],
+                        ],
+                      ),
+                    ],
+                    pw.SizedBox(height: 12), // Add space between customers
+                    pw.Divider(),
                   ],
-                  pw.Divider(),
                 ],
-              ],
+              ),
             ),
-          );
+          ];
         },
       ),
     );
 
-    // Get the directory to save the PDF
+    // Get the directory to save the PDF and create a 'PDFs' folder if it doesn't exist
     final outputDir = await getApplicationDocumentsDirectory();
-    final file = File('${outputDir.path}/Customer_List.pdf');
+    final pdfDir = Directory('${outputDir.path}/PDFs');
 
-    // Save the PDF to the file
+    if (!await pdfDir.exists()) {
+      await pdfDir.create(
+          recursive: true); // Create the directory if it doesn't exist
+    }
+
+    // Create a file name with the current date
+    final fileName = 'Customer_List_$date.pdf';
+
+    // Save the PDF in the 'PDFs' directory
+    final file = File('${pdfDir.path}/$fileName');
+
+    // Write the PDF content to the file
     await file.writeAsBytes(await pdf.save());
 
     // Inform the user that the PDF has been saved
